209f4b4439cabbc1cefe59edf543532dad4a5677
naciter_hao <775266751@qq.com>
2021-04-24 22:48:29 +0800

    20200424

--- src/leetcode/At3.java
+++ src/leetcode/At3.java
@@ -1,22 +1,31 @@
 package leetcode;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * @author: 言叶长琴
  */
 public class At3 {
 
     public static void main(String[] args) {
-
+        System.out.println(lengthOfLongestSubstring("aabcdcde"));
     }
 
     public static int lengthOfLongestSubstring(String s) {
-        int val = Integer.MIN_VALUE;
-        for(int i = 0; i<s.length();i++) {
-            for(int j = 0; j < i; j++) {
-
+        if (s.length() == 0) return 0;
+        int res = 0;
+        Map<Character, Integer> map = new HashMap<>();
+        int start = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if (map.containsKey(s.charAt(i))) {
+                start = Math.max(start, map.get(s.charAt(i)) + 1);
             }
+            res = Math.max(res, i - start + 1);
+            map.put(s.charAt(i), i);
         }
-        return 0;
+        return res;
     }
+
 
 }
