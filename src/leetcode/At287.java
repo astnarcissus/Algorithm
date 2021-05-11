16d26bfb11e5c706953ffa7d3bb598849b7b3f6d
xuh <hxu@ittx.com.cn>
2021-05-11 17:10:29 +0800

    20210511

--- src/leetcode/At287.java
+++ src/leetcode/At287.java
@@ -113,4 +113,33 @@ public class At287 {
         return left;
     }
 
+    /**
+     * 二进制的解法，真特么是个人才，我都不想看
+     *
+     * @param nums
+     * @return
+     */
+    public int findDuplicate4(int[] nums) {
+        int n = nums.length, ans = 0;
+        int bit_max = 31;
+        while (((n - 1) >> bit_max) == 0) {
+            bit_max -= 1;
+        }
+        for (int bit = 0; bit <= bit_max; ++bit) {
+            int x = 0, y = 0;
+            for (int i = 0; i < n; ++i) {
+                if ((nums[i] & (1 << bit)) != 0) {
+                    x += 1;
+                }
+                if (i >= 1 && ((i & (1 << bit)) != 0)) {
+                    y += 1;
+                }
+            }
+            if (x > y) {
+                ans |= 1 << bit;
+            }
+        }
+        return ans;
+    }
+
 }
