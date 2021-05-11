19a93893bc9675b81a86d15a9e285f9779472591
xuh <hxu@ittx.com.cn>
2021-05-11 15:30:43 +0800

    20210511

--- src/leetcode/Test.java
+++ src/leetcode/Test.java
@@ -1,13 +1,22 @@
 package leetcode;
 
+import java.util.ArrayList;
+
 /**
  * @author: 言叶长琴
  */
 public class Test {
 
     public static void main(String[] args) {
-        System.out.println(0x7fffffff);
-        System.out.println(0x7fffffff >> 1);
+        ArrayList<Integer> list1 = new ArrayList<>();
+        list1.add(1);
+        list1.add(2);
+        ArrayList<Integer> list2 = new ArrayList<>();
+
+        list2.addAll(list1);
+        list2 = list1;
+        System.out.println(list2);
+
     }
 
 }
