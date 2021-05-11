3672c9c30542483f015c5f64a7edf99e06cab5bb
naciter_hao <775266751@qq.com>
2021-05-11 22:55:36 +0800

    20210511

--- src/leetcode/At31.java
+++ src/leetcode/At31.java
@@ -4,33 +4,36 @@ import java.util.Arrays;
 
 /**
  * 实现获取 下一个排列 的函数，算法需要将给定数字序列重新排列成字典序中下一个更大的排列。
- *
+ * <p>
  * 如果不存在下一个更大的排列，则将数字重新排列成最小的排列（即升序排列）。
- *
+ * <p>
  * 必须 原地 修改，只允许使用额外常数空间。
- *
+ * <p>
  * 示例 1：
- *
+ * <p>
  * 输入：nums = [1,2,3]
  * 输出：[1,3,2]
  * 示例 2：
- *
+ * <p>
  * 输入：nums = [3,2,1]
  * 输出：[1,2,3]
  * 示例 3：
- *
+ * <p>
  * 输入：nums = [1,1,5]
  * 输出：[1,5,1]
  * 示例 4：
- *
+ * <p>
  * 输入：nums = [1]
  * 输出：[1]
- *
  */
 public class At31 {
 
     public static void main(String[] args) {
-        new At31().nextPermutation(new int[]{1,1,3});
+        int[] arr = {3, 2, 1};
+        new At31().nextPermutation2(arr);
+        for (int i : arr) {
+            System.out.println(i);
+        }
     }
 
     /**
@@ -44,7 +47,7 @@ public class At31 {
      */
     public void nextPermutation(int[] nums) {
         boolean flg = false;
-        for(int i = nums.length - 1; i >0; i--) {
+        for (int i = nums.length - 1; i > 0; i--) {
             for (int j = i - 1; j >= 0; j--) {
                 if (nums[j] < nums[i] && !flg) {
                     flg = true;
@@ -56,6 +59,66 @@ public class At31 {
         }
 
         if (!flg) Arrays.sort(nums);
+    }
+
+    /**
+     * 正确的解法
+     * 推导
+     * 我们希望下一个数比当前数大，这样才满足“下一个排列”的定义。因此只需要将后面的「大数」与前面的「小数」交换，就能得到一个更大的数。比如 123456，将 5 和 6 交换就能得到一个更大的数 123465。
+     * 我们还希望下一个数增加的幅度尽可能的小，这样才满足“下一个排列与当前排列紧邻“的要求。为了满足这个要求，我们需要：
+     * 在尽可能靠右的低位进行交换，需要从后向前查找
+     * 将一个 尽可能小的「大数」 与前面的「小数」交换。比如 123465，下一个排列应该把 5 和 4 交换而不是把 6 和 4 交换
+     * 将「大数」换到前面后，需要将「大数」后面的所有数重置为升序，升序排列就是最小的排列。以 123465 为例：首先按照上一步，交换 5 和 4，得到 123564；然后需要将 5 之后的数重置为升序，得到 123546。显然 123546 比 123564 更小，123546 就是 123465 的下一个排列
+     *
+     * 过程：
+     * 从后向前查找第一个相邻升序的元素对 (i,j)，满足 A[i] < A[j]。此时 [j,end) 必然是降序
+     * 在 [j,end) 从后向前查找第一个满足 A[i] < A[k] 的 k。A[i]、A[k] 分别就是上文所说的「小数」、「大数」
+     * 将 A[i] 与 A[k] 交换
+     * 可以断定这时 [j,end) 必然是降序，逆置 [j,end)，使其升序
+     * 如果在步骤 1 找不到符合的相邻元素对，说明当前 [begin,end) 为一个降序顺序，则直接跳到步骤 4
+     *
+     * @param nums
+     */
+    public void nextPermutation2(int[] nums) {
+        int n = nums.length;
+        if (n <= 1) return;
+        int i = n - 2;
+        int j = n - 1;
+        while (i > 0 && nums[i] >= nums[j])  {
+            i--;
+            j--;
+        }
+        System.out.println("i = " + i);
+        System.out.println("j = " + j);
+        if (nums[i] >= nums[j]) {
+            sort(i, nums);
+            return;
+        }
+        int k = j;
+        for (int x = j + 1; x < n; x++) {
+            if (nums[x] > nums[i] && nums[x] < nums[j]) {
+                k = x;
+            }
+        }
+        swap(i, k, nums);
+        sort(j, nums);
+
+    }
+
+    public void sort(int start, int[] nums) {
+        for (int i = start; i < nums.length - 1; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] > nums[j]) {
+                    swap(i, j, nums);
+                }
+            }
+        }
+    }
+
+    public void swap(int i, int j, int[] nums) {
+        int tmp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = tmp;
     }
 
 }
