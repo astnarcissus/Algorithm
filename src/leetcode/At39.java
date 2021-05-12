e8bf876ad4ae08b8b7b466c8f33f2f067f83826f
xuh <hxu@ittx.com.cn>
2021-05-12 16:27:47 +0800

    20210512

--- src/leetcode/At39.java
+++ src/leetcode/At39.java
@@ -1,8 +1,6 @@
 package leetcode;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 /**
  * 给定一个无重复元素的数组candidates和一个目标数target，找出candidates中所有可以使数字和为target的组合。
@@ -65,6 +63,44 @@ public class At39 {
             nowSum -= candidates[cur];
             nowList.remove(nowList.size() - 1);
             dfs(cur + 1, candidates, nowSum, nowList, lists, target, n);
+        }
+    }
+
+    /**
+     * 别人的解法，我应该也剪枝了呀，很奇怪呢，难道是因为我的树太深了？
+     *
+     * @param candidates
+     * @param target
+     * @return
+     */
+    public List<List<Integer>> combinationSum2(int[] candidates, int target) {
+        int len = candidates.length;
+        List<List<Integer>> res = new ArrayList<>();
+        if (len == 0) {
+            return res;
+        }
+
+        Arrays.sort(candidates);
+        Deque<Integer> path = new ArrayDeque<>();
+        dfs2(candidates, 0, len, target, path, res);
+        return res;
+    }
+
+    private void dfs2(int[] candidates, int begin, int len, int target, Deque<Integer> path, List<List<Integer>> res) {
+        if (target == 0) {
+            res.add(new ArrayList<>(path));
+            return;
+        }
+
+        for (int i = begin; i < len; i++) {
+            if (target - candidates[i] < 0) {
+                break;
+            }
+
+            path.addLast(candidates[i]);
+
+            dfs2(candidates, i, len, target - candidates[i], path, res);
+            path.removeLast();
         }
     }
 
