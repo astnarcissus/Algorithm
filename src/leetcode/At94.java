7e6db8d7e4bec0576fb97151836a6ce09d0e05ff
xuh <hxu@ittx.com.cn>
2021-05-17 19:00:30 +0800

    20210517

--- src/leetcode/At94.java
+++ src/leetcode/At94.java
@@ -2,8 +2,7 @@ package leetcode;
 
 import leetcode.common.TreeNode;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 /**
  * @author: 言叶长琴
@@ -42,7 +41,67 @@ public class At94 {
         dfs(root.left, list);
         list.add(root.val);
         dfs(root.right, list);
+    }
 
+    /**
+     * 迭代
+     *
+     * @param root
+     * @return
+     */
+    public List<Integer> inorderTraversal1(TreeNode root) {
+        List<Integer> res = new ArrayList<Integer>();
+        Deque<TreeNode> stk = new LinkedList<TreeNode>();
+        while (root != null || !stk.isEmpty()) {
+            while (root != null) {
+                stk.push(root);
+                root = root.left;
+            }
+            root = stk.pop();
+            res.add(root.val);
+            root = root.right;
+        }
+        return res;
+    }
+
+
+    /**
+     * morris遍历 算法
+     * <p>
+     * 如果 x 无左孩子，先将 x 的值加入答案数组，再访问 x 的右孩子，即 x=x.right。
+     * 如果 x 有左孩子，则找到 x 左子树上最右的节点
+     * （即左子树中序遍历的最后一个节点，x 在中序遍历中的前驱节点），我们记为 predecessor。根据 predecessor 的右孩子是否为空，进行如下操作。
+     * 如果 predecessor 的右孩子为空，则将其右孩子指向 x，然后访问 x 的左孩子，即 x=x.left。
+     * 如果 predecessor 的右孩子不为空，则此时其右孩子指向 x，说明我们已经遍历完 x 的左子树，我们将 predecessor 的右孩子置空，将 x 的值加入答案数组，然后访问 x 的右孩子，即 x=x.right。
+     *
+     * @param root
+     * @return
+     */
+    public List<Integer> inorderTraversal2(TreeNode root) {
+        List<Integer> list = new ArrayList<>();
+        TreeNode predecessor = null;
+
+        while (root != null) {
+            if (root.left != null) {
+                predecessor = root.left;
+                while (predecessor.right != null && predecessor.right != root) {
+                    predecessor = predecessor.right;
+                }
+                if (predecessor.right == null) {
+                    predecessor.right = root;
+                    root = root.left;
+                } else {
+                    list.add(root.val);
+                    predecessor.right = null;
+                    root = root.right;
+                }
+            } else {
+                list.add(root.val);
+                root = root.right;
+            }
+        }
+
+        return list;
     }
 
 
