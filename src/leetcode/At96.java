4bb03e94163a3660faa1984e6bfecfb6bc11c91a
xuh <hxu@ittx.com.cn>
2021-05-17 13:17:34 +0800

    20210517

---
+++ src/leetcode/At96.java
@@ -0,0 +1,55 @@
+package leetcode;
+
+/**
+ * 给你一个整数 n ，求恰由 n 个节点组成且节点值从 1 到 n 互不相同的 二叉搜索树 有多少种？返回满足题意的二叉搜索树的种数。
+ * <p>
+ * 示例 1：
+ * 输入：n = 3
+ * 输出：5
+ * <p>
+ * 示例 2：
+ * 输入：n = 1
+ * 输出：1
+ */
+public class At96 {
+
+    public static void main(String[] args) {
+        System.out.println(new At96().numTrees(5));
+    }
+
+    /**
+     * 这题和数学有点关系，诶，找规律什么的可真是难为我了
+     * <p>
+     * 官方题解方法一
+     *
+     * @param n
+     * @return
+     */
+    public int numTrees(int n) {
+        int[] G = new int[n + 1];
+        G[0] = 1;
+        G[1] = 1;
+
+        for (int i = 2; i <= n; ++i) {
+            for (int j = 1; j <= i; ++j) {
+                G[i] += G[j - 1] * G[i - j];
+            }
+        }
+        return G[n];
+    }
+
+    /**
+     * 官方题解二 推导公式
+     * @param n
+     * @return
+     */
+    public int numTrees2(int n) {
+        // 提示：我们在这里需要用 long 类型防止计算过程中的溢出
+        long C = 1;
+        for (int i = 0; i < n; ++i) {
+            C = C * 2 * (2 * i + 1) / (i + 2);
+        }
+        return (int) C;
+    }
+
+}
