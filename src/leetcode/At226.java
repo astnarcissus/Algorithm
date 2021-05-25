a0652d6614744edee0ae349b6128f9c2e10a3db4
xuh <hxu@ittx.com.cn>
2021-05-25 21:23:18 +0800

    20210525

---
+++ src/leetcode/At226.java
@@ -0,0 +1,42 @@
+package leetcode;
+
+import leetcode.common.TreeNode;
+
+/**
+ * 翻转一棵二叉树。
+ *
+ * 示例：
+ *
+ * 输入：
+ *
+ *      4
+ *    /   \
+ *   2     7
+ *  / \   / \
+ * 1   3 6   9
+ * 输出：
+ *
+ *      4
+ *    /   \
+ *   7     2
+ *  / \   / \
+ * 9   6 3   1
+ *
+ */
+public class At226 {
+
+    /**
+     * 没难度，不愧是简单题
+     * @param root
+     * @return
+     */
+    public TreeNode invertTree(TreeNode root) {
+        if (root == null) return null;
+        TreeNode tmp = root.left;
+        root.left = invertTree(root.right);
+        root.right = invertTree(tmp);
+        return root;
+    }
+
+
+}
