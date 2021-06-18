aadde456bae67fe780aeb831d15d89b7095c8156
naciter_hao <775266751@qq.com>
2021-06-19 03:14:18 +0800

    20210619

--- src/leetcode/At300.java
+++ src/leetcode/At300.java
@@ -25,6 +25,7 @@ public class At300 {
     /**
      * 动态规划，和我想的差不多其实
      * 时间复杂度 O(n²)
+     *
      * @param nums
      * @return
      */
@@ -40,10 +41,42 @@ public class At300 {
                     dp[i] = Math.max(dp[i], dp[j] + 1);
                 }
             }
-            res = Math.max(res,dp[i]);
+            res = Math.max(res, dp[i]);
         }
         return res;
     }
 
-
+    /**
+     * 动态规划+二分查找
+     * 具体实现那块还是对二分有点小烦，诶
+     *
+     * 很具小巧思。新建数组 cell，用于保存最长上升子序列。
+     * <p>
+     * 对原序列进行遍历，将每位元素二分插入 cell 中。
+     * <p>
+     * 如果 cell 中元素都比它小，将它插到最后
+     * 否则，用它覆盖掉比它大的元素中最小的那个。
+     * 总之，思想就是让 cell 中存储比较小的元素。这样，cell 未必是真实的最长上升子序列，但长度是对的。
+     *
+     * @param nums
+     * @return
+     */
+    public int lengthOfLIS2(int[] nums) {
+        int res = 0;
+        int n = nums.length;
+        int[] tail = new int[n];
+        for (int num : nums) {
+            int l = 0;
+            int r = res;
+            while (l < r) {
+                int mid = l + (r - l ) / 2;
+                if (tail[mid] < num) l = mid + 1;
+                else r = mid;
+            }
+            tail[l] = num;
+            if (l == res)
+                ++res;
+        }
+        return res;
+    }
 }
